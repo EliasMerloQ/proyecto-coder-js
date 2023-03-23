93ff80cf5057eb1d59ca735fab6aab369f2e1c76
elias.merlo <elias.merlo.q@gmail.com>
2023-03-22 21:24:58 -0300

    update js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function check_ab(caracter1) {
   }
 }
 function check_abc(caracter2){
-  while(caracter2 != "a" && caracter2 != "b" && caracter2 != "c"){
+  while(caracter2 !== "a" && caracter2 !== "b" && caracter2 !== "c"){
     caracter2 = prompt("Caracter ingresado no valido \nIngrese (a), (b) o (c) ")      
   }
 }
@@ -33,10 +33,10 @@ switch(_class) {
     alert(equipo2)
     select3 = prompt("(a) (1) Armadura de cuero tachonado \n (b) (1) Cota de escamas")
     check_ab(select3);
-    if(select3 = "a"){
+    if(select3 == "a"){
       equipo3 = "(1) Armadura de cuero tachonado"
     }
-    if(select3 = "b"){
+    if(select3 == "b"){
       equipo3 = "(1) Cota de escamas"
     }
     equipo4 = "(1) Herramientas de ladron \n (1) Paquete de Explorador"
@@ -46,18 +46,18 @@ switch(_class) {
     alert("Seleccione su equipo a continuación \n Elija ingresando a,b,c... \n el numero (n) indica la cantidad de objetos a dar")
     select1 = prompt ("(a) (1) Gran Hacha \n(b) (1) Arma marcial")
     check_ab(select1)
-    if(select1 = "a") {
+    if(select1 == "a") {
       equipo1 = "(1) Gran Hacha"
     }
-    if(select1 = "b") {
+    if(select1 == "b") {
       equipo1 = "(1) Arma Marcial";
     }
     select2 = prompt("(a) (2) Hacha de mano \n (b) (1) Arma simple")
     check_ab(select2)
-    if(select2 = "a") {
+    if(select2 == "a") {
       equipo2 = "(2) Hacha de mano"
     }
-    if(select2 = "b") {
+    if(select2 =="b") {
       equipo2 = "(1) Arma simple";
     }
     equipo3 = "(1) Equipo de explorador \n(4) Javalinas"
@@ -69,22 +69,24 @@ switch(_class) {
     alert("Seleccione su equipo a continuación \n Elija ingresando a,b,c... \n el numero (n) indica la cantidad de objetos a dar")
     select1 = prompt("(a) (1) Estoque \n(b) (1) Espada Larga \n (c) (1) Arma simple")
     check_abc(select1)
-    if(select1 = "a"){
-      equipo1 = "(1) Estoque"
+    console.log(select1)
+    if(select1 == "a") {
+      equipo1 = "(1) Estoque";
     }
-    if(select1 = "b"){
+    else if(select1 == "b") {
       equipo1 = "(1) Espada Larga"
     }
-    if(select1 = "c"){
+    else if(select1 == "c") {
       equipo1 = "(1) Arma simple"
     }
+    console.log(equipo1)
     select2 = prompt("(a) (1) Paquete de diplomatico \n(b) (1) Paquete de artista");
     check_ab(select2)
     if(select2 = "a"){
-      equipo2 = "(1) Paquete de diplomatico"
+      equipo2 = "(1) Paquete de diplomatico";
     }
-    if(select2 = "b"){
-      equipo2 = "(1) Paquete de artista"
+    else if(select2 = "b"){
+      equipo2 = "(1) Paquete de artista";
     }
     equipo3 = "(1) Instrumento musical"
     alert("(1) Instrumento musical")
