f1236247649dec63890898cc0b0b3025726882bc
elias.merlo <elias.merlo.q@gmail.com>
2023-04-19 01:28:05 -0300

    Login & Armeria

--- js/main.js
+++ js/main.js
@@ -1,108 +1,7 @@
-/*function ticket(name,equip1,equip2,equip3,equip4) {
-  alert("Bienvenido al gremio " + name + "\n Su equipo inicial es el siguiente: \n" + equip1 + "\n" + equip2 + "\n" + equip3 + "\n" + equip4) 
-  alert("Pase a la Tienda de Colombus con el siguiente ticket a reclamar sus objetos gratis para su aventura!")
-}
-function check_clase(caracter){
-  while(caracter !== "1" && caracter !== "2" && caracter !== "3"){
-    caracter = prompt("Caracter ingresado no valido Ingrese: \n1) Artificiero\n2) Bárbaro \n3) Bardo")
-  }
-  return caracter
-}
-function check_ab(caracter) {
-  while(caracter != "a" && caracter != "b"){
-    caracter = prompt("Caracter ingresado no valido \nIngrese (a) o (b)")      
-  }
-  return caracter
-}
-function check_abc(caracter){
-  while(caracter !== "a" && caracter !== "b" && caracter !== "c"){
-    caracter = prompt("Caracter ingresado no valido \nIngrese (a), (b) o (c) ")      
-  }
-  return caracter
-}
-
-let nombre = prompt("Ingrese su nombre");
-alert("Bienvenido " + nombre +"!");
-let clase = prompt("Digame cual es su Clase \n (elija uno de los numeros mostrados a continuacion) \n1) Artificiero \n2) Bárbaro \n3) Bardo");
-let _class = check_clase(clase);
-
-switch(_class) {
-  case "1":
-    alert("Seleccione su equipo a continuación \n Elija ingresando a,b,c... \n el numero (n) indica la cantidad de objetos a dar")
-    equipo1 = "(2) Armas simples"
-    alert(equipo1)
-    equipo2 = "(1) Ballesta ligera \n(20) Virotes"
-    alert(equipo2)
-    select3 = prompt("(a) (1) Armadura de cuero tachonado \n (b) (1) Cota de escamas")
-    select3 = check_ab(select3);
-    if(select3 == "a"){
-      equipo3 = "(1) Armadura de cuero tachonado"
-    }
-    if(select3 == "b"){
-      equipo3 = "(1) Cota de escamas"
-    }
-    equipo4 = "(1) Herramientas de ladron \n (1) Paquete de Explorador"
-    break
-
-  case "2":
-    alert("Seleccione su equipo a continuación \n Elija ingresando a,b,c... \n el numero (n) indica la cantidad de objetos a dar")
-    select1 = prompt ("(a) (1) Gran Hacha \n(b) (1) Arma marcial")
-    select1 = check_ab(select1)
-    if(select1 == "a") {
-      equipo1 = "(1) Gran Hacha"
-    }
-    if(select1 == "b") {
-      equipo1 = "(1) Arma Marcial";
-    }
-    select2 = prompt("(a) (2) Hacha de mano \n (b) (1) Arma simple")
-    select2 = check_ab(select2)
-    if(select2 == "a") {
-      equipo2 = "(2) Hacha de mano"
-    }
-    if(select2 =="b") {
-      equipo2 = "(1) Arma simple";
-    }
-    equipo3 = "(1) Equipo de explorador \n(4) Javalinas"
-    alert(equipo3)
-    equipo4 = ""
-    break;
-
-  case "3":
-    alert("Seleccione su equipo a continuación \n Elija ingresando a,b,c... \n el numero (n) indica la cantidad de objetos a dar")
-    select1 = prompt("(a) (1) Estoque \n(b) (1) Espada Larga \n (c) (1) Arma simple")
-    select1 = check_abc(select1)
-    if(select1 == "a") {
-      equipo1 = "(1) Estoque";
-    }
-    else if(select1 == "b") {
-      equipo1 = "(1) Espada Larga"
-    }
-    else if(select1 == "c") {
-      equipo1 = "(1) Arma simple"
-    }
-    console.log(equipo1)
-    select2 = prompt("(a) (1) Paquete de diplomatico \n(b) (1) Paquete de artista");
-    select2 = check_ab(select2)
-    if(select2 = "a"){
-      equipo2 = "(1) Paquete de diplomatico";
-    }
-    else if(select2 = "b"){
-      equipo2 = "(1) Paquete de artista";
-    }
-    equipo3 = "(1) Instrumento musical"
-    alert("(1) Instrumento musical")
-    equipo4 = "(1) Armadura de cuero \n(1) Daga"
-    alert("(1) Armadura de cuero \n(1) Daga")
-    break
-}
-
-ticket(nombre,equipo1,equipo2,equipo3,equipo4)*/
-
 //Codigo para segunda entrega. El codigo de arriba hay que adaptarlo para uso futuro.
-alert("Bienvenido a la tienda de Colombus! \nPase a ver el inventario!")
 // Constructor de Armas
 class Weapons{
-  constructor(name, damage, properties, weight, cost, type,){
+  constructor(name, damage, properties, weight, cost, type){
     this.name = name
     this.damage = damage
     this.properties = properties
@@ -181,72 +80,99 @@ const picoDeGuerra = new Weapons("Pico de guerra", "1d8 perforante", "-", "2 lb.
 const tridente = new Weapons("Tridente", "1d6 perforante", "Arrojadiza (alcance 20/60), versátil (1d8)", "4 lb.", "5 po", "marcial");
 
 //Armaduras instanciadas como objetos
-const acolchada = new Armors("Acolchada", "CA 11 + Des", "Desventaja", "-", "8 lb.", "5 po", "armadura ligera");
-const cuero = new Armors("Cuero", "CA 11 + Des", "-", "-", "10 lb.", "10 po", "armadura ligera");
-const cueroTachonado = new Armors("Cuero tachonado", "CA 12 + Des", "Desventaja", "-", "13 lb.", "45 po", "armadura ligera");
-const camisaDeMalla = new Armors("Camisa de malla", "CA 13 + Des (máx. 2)", "-", "-", "20 lb.", "50 po", "armadura media");
-const cotaDeEscamas = new Armors("Cota de escamas", "CA 14 + Des (máx. 2)", "Desventaja", "-", "20 lb.", "50 po", "armadura media");
-const coraza = new Armors("Coraza", "CA 14 + Des (máx. 2)", "-", "-", "20 lb.", "400 po", "armadura media");
-const placasMalla = new Armors("Placas y malla", "CA 15 + DES(máx. 2)", "-", "Desventaja", "40 lb.", "750 po", "armadura media")
-const pieles = new Armors("Pieles", "CA 12 + Des (máx. 2)", "-", "-", "12 lb.", "10 po", "armadura media");
-const armaduraDeBandas = new Armors("Armadura de bandas", "CA 17", "Fue 15", "Desventaja", "60 lb.", "200 po", "armadura pesada");
-const armaduraDePlacas = new Armors("Armadura de placas", "CA 18", "Fue 15", "Desventaja", "65 lb.", "1500 po", "armadura pesada");
-const cotaGuarnecida = new Armors("Cota guarnecida", "CA 14", "-", "Desventaja", "40 lb.", "30 po", "armadura pesada")
-const cotaDeMalla = new Armors("Cota de malla", "CA 16", "Fue 13", "Desventaja", "55 lb.", "75 po", "armadura pesada");
+const acolchada = new Armors("Acolchada", "CA 11 + DES", "Desventaja", "-", "8 lb.", "5 po", "armadura ligera");
+const cuero = new Armors("Cuero", "CA 11 + DES", "-", "-", "10 lb.", "10 po", "armadura ligera");
+const cueroTachonado = new Armors("Cuero tachonado", "CA 12 + DES", "Desventaja", "-", "13 lb.", "45 po", "armadura ligera");
+const camisaDeMalla = new Armors("Camisa de malla", "CA 13 + DES (máx. 2)", "-", "-", "20 lb.", "50 po", "armadura media");
+const cotaDeEscamas = new Armors("Cota de escamas", "CA 14 + DES (máx. 2)", "Desventaja", "-", "20 lb.", "50 po", "armadura media");
+const coraza = new Armors("Coraza", "CA 14 + DES (máx. 2)", "-", "-", "20 lb.", "400 po", "armadura media");
+const placasMalla = new Armors("Placas y malla", "CA 15 + DES(máx. 2)", "Desventaja", "-", "40 lb.", "750 po", "armadura media")
+const pieles = new Armors("Pieles", "CA 12 + DES (máx. 2)", "-", "-", "12 lb.", "10 po", "armadura media");
+const armaduraDeBandas = new Armors("Armadura de bandas", "CA 17", "Desventaja", "Fue 15", "60 lb.", "200 po", "armadura pesada");
+const armaduraDePlacas = new Armors("Armadura de placas", "CA 18", "Desventaja", "Fue 15", "65 lb.", "1500 po", "armadura pesada");
+const cotaGuarnecida = new Armors("Cota guarnecida", "CA 14", "Desventaja", "-", "40 lb.", "30 po", "armadura pesada")
+const cotaDeMalla = new Armors("Cota de malla", "CA 16", "Desventaja", "Fue 13", "55 lb.", "75 po", "armadura pesada");
 
 
 // Array de todas las armas
 const weaponList = [baston, clava, daga, granClava, hachaDeMano, hoz, javalina, lanza, martilloLigero, maza,arcoCorto, ballestaLigera, dardo, honda, alabarda, cimitarra, espadaCorta, espadaLarga, estoque, granHacha, guja, hachaDeBatalla, lanzaDeCaballeria, latigo, luceroDelAlba, mandoble, mangual, martilloDeGuerra, mazoDeGuerra, pica, picoDeGuerra, tridente]
 //Array de todas las armaduras
 const armorList = [acolchada, cuero, cueroTachonado, camisaDeMalla, cotaDeEscamas, coraza,placasMalla ,pieles, armaduraDeBandas, armaduraDePlacas, cotaGuarnecida, cotaDeMalla];
-// Recorro el Array 
-for (const Weapons of weaponList) {
-  console.log(Weapons)
-}
-// Checkeo por consola que estandarice el valor de po
-for (const Weapons of weaponList) {
-  console.log(Weapons.name + " " + Weapons.cost + " " + Weapons.moneyValue());
-}
+
 // Filtrar las armas que tienen la propiedad "type" igual a "simple"
 const weaponsListSimple = weaponList.filter((arma) => arma.type === "simple");
-console.log(weaponsListSimple);
+
 // Filtrar las armas que tienen la propiedad "type" igual a "marcial"
 const weaponsListMarcial = weaponList.filter((arma) => arma.type === "marcial");
-console.log(weaponsListMarcial);
 
 // Filtrar las armaduras que tienen la propiedad "type" igual a "armadura ligera"
 const armorListLight = armorList.filter((armadura) => armadura.type === "armadura ligera");
-console.log(armorListLight);
+
 // Filtrar las armaduras que tienen la propiedad "type" igual a "armadura media"
 const armorListMedium = armorList.filter((armadura) => armadura.type === "armadura media");
-console.log(armorListMedium);
+
 // Filtrar las armaduras que tienen la propiedad "type" igual a "armadura pesada"
 const armorListHeavy = armorList.filter((armadura) => armadura.type === "armadura pesada");
-console.log(armorListHeavy);
-//Interactivo
-function check(armorOrWeapon) {
-  while(armorOrWeapon!= "armaduras" && armorOrWeapon != "armas"){
-    alert("Lo siento, no puedo mostrar ese tipo de inventario."); 
-    armorOrWeapon = prompt("¿Quiere ver las armas o las armaduras?");      
-  }
-  return armorOrWeapon
+
+//--------//
+
+function agregarArma(arma){
+  const ListaArmas = document.getElementById("armasLista")
+  ListaArmas.innerHTML +=`
+            <div class="row-link-content p-1">
+              <div class="row">
+                <div class="col-6 col-md-3">
+                  <strong class="liColor">${arma.name}</strong>
+                </div>
+                <div class="col-6 col-md-3">
+                  <strong class="liColor">${arma.damage}</strong>
+                </div>
+                <div class="col-6 col-md-3">
+                  <strong class="liColor">${arma.properties}</strong>
+                </div>
+                <div class="col-1 col-md-1">
+                  <strong class="liColor">${arma.weight}</strong>
+                </div>
+                <div class="col-1 col-md-1">
+                  <strong class="liColor">${arma.cost}</strong>
+                </div>
+              </div>
+              </div>
+            <hr style = "color: #fff"> 
+            </div>` 
 }
-let inventoryType = prompt("¿Quiere ver las armas o las armaduras?");
-let inventoryTypeChek = check(inventoryType);
-
-if (inventoryTypeChek.toLowerCase() === "armas") {
-  let weaponString = "Las armas disponibles son:\n";
-   for (let i = 0; i < weaponList.length; i++) {
-    weaponString += `- ${weaponList[i].name} (${weaponList[i].type}) - ${weaponList[i].moneyValue()}\n`;
-  }
-  alert(weaponString);
-} 
-else if (inventoryTypeChek.toLowerCase() === "armaduras") {
-  let armorString = "Las armaduras disponibles son:\n";
-  for (let i = 0; i < armorList.length; i++) {
-    armorString += `- ${armorList[i].name} (${armorList[i].type}) - ${armorList[i].moneyValue()}\n`;
-  }
-  alert(armorString);
-}  
-
-
+for (let i = 0; i < weaponList.length; i++) {
+  agregarArma(weaponList[i]);
+}
+function agregarArmadura(armadura){
+  const ListaArmaduras = document.getElementById("armadurasLista")
+  ListaArmaduras.innerHTML +=`
+             <div class="row-link-content p-1">
+              <div class="row">
+                <div class="col-6 col-md-3">
+                  <strong class="liColor">${armadura.name}</strong>
+                </div>
+                <div class="col-6 col-md-3">
+                  <strong class="liColor">${armadura.ac}</strong>
+                </div>
+                <div class="col-3 col-md-2">
+                  <strong class="liColor">${armadura.stealth}</strong>
+                </div>
+                <div class="col-3 col-md-1">
+                  <strong class="liColor">${armadura.strength}</strong>
+                </div>
+                <div class="col-1 col-md-1">
+                  <strong class="liColor">${armadura.weight}</strong>
+                </div>
+                <div class="col-1 col-md-1">
+                  <strong class="liColor">${armadura.cost}</strong>
+                </div>
+              </div>
+              </div>
+            <hr style = "color: #fff"> 
+            </div>` 
+}
+for (let i = 0; i < armorList.length; i++) {
+  agregarArmadura(armorList[i]);
+  console.log(armorList[i])
+}
